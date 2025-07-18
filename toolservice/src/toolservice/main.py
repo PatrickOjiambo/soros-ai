13853ee78e2a30b787e40ec9bd829ec36dd4fd06
PatrickOjiambo <pashrick237@gmail.com>
2025-07-18 15:34:19 +0300

    feat: moved server port to env

--- toolservice/src/toolservice/main.py
+++ toolservice/src/toolservice/main.py
@@ -1,11 +1,14 @@
 from concurrent import futures
 import logging
+import os
 import grpc
 from grpc_health.v1 import health, health_pb2, health_pb2_grpc
 from grpc_reflection.v1alpha import reflection
 from toolservice.generated import rpc_pb2_grpc, rpc_pb2
 from toolservice.toolcall_service import ToolCallerService
 
+# Get the port from an environment variable, with a default value
+SERVER_PORT = os.environ.get("SERVER_PORT", "50051")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -22,9 +25,9 @@ def serve():
         reflection.SERVICE_NAME,
     )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port(f"[::]:{SERVER_PORT}")
     server.start()
-    logging.info("Server started on port 50051")
+    logging.info(f"Server started on port {SERVER_PORT}")
     try:
         server.wait_for_termination()
     except KeyboardInterrupt:
